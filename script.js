6453d015a932f65b610b05821795a51d70b08be2
Leticia Cordeiro <lehcs913@gmail.com>
2025-05-21 16:40:16 -0300

    função de remover no botão

--- script.js
+++ script.js
@@ -33,3 +33,12 @@ document.getElementById("add").addEventListener("click",() => {
         clientes.appendChild(cadastro);
     });
 });
+function remove(botao, id) {
+    fetch(`https://crudcrud.com/api/bf30286168b845328c0bf63122a9ff28/listadeclientes/${id}`, {
+        method: "DELETE"
+    })
+    .then(() => {
+        botao.parentElement.remove();
+    })
+    .catch(erro => alert.("Erro ao deletar:", erro));
+}
